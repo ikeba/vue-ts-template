0bddc9ddf5bd1b344d46c00da02361cad1134baa
ikeba <illia.keba@gmail.com>
2023-10-31 23:09:25 +0100

    initial commit

---
+++ .eslint.js
@@ -0,0 +1,44 @@
+module.exports = {
+    root: true,
+    env: {
+        browser: true,
+        es2021: true,
+        node: true,
+    },
+    extends: [
+        'plugin:vue/vue3-recommended',
+        'eslint:recommended',
+        'airbnb-base',
+        'airbnb-typescript/base',
+        '@vue/typescript/recommended',
+        '@vue/prettier',
+        '@vue/prettier/@typescript-eslint',
+    ],
+    parserOptions: {
+        ecmaVersion: 2021,
+        project: './tsconfig.json',
+    },
+    rules: {
+        'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        '@typescript-eslint/interface-name-prefix': 'off',
+        '@typescript-eslint/explicit-function-return-type': 'off',
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/no-non-null-assertion': 'off',
+        '@typescript-eslint/no-unused-vars': [
+            'off',
+            { argsIgnorePattern: '^_' },
+        ],
+        'prettier/prettier': ['error', {}, { usePrettierrc: true }],
+        quotes: [2, 'single', { avoidEscape: true }],
+        '@typescript-eslint/quotes': [
+            'error',
+            'single',
+            {
+                avoidEscape: true,
+                allowTemplateLiterals: true,
+            },
+        ],
+    },
+};
---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+    console.log('Hello world');
+</script>
+
+<template>
+    <div></div>
+</template>
+
+<style scoped></style>
